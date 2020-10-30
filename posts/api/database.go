1ca49208e55d5aebbb24b8e1d4cb64cd2704076d
Ari Rigan <ari.rigan@gmail.com>
2020-10-30 08:50:01 -0700

    post database connection string fix

--- posts/api/database.go
+++ posts/api/database.go
@@ -18,13 +18,13 @@ func InitDB() *sql.DB {
 	var err error
 	
 	// We've decided to give the connection string for the rest of the microservices
-	DB, err = sql.Open("mysql", "root:root@tcp(172.28.1.3:3306)/postsDB?parseTime=true")
+	DB, err = sql.Open("mysql", "root:root@tcp(172.28.1.2:3306)/postsDB?parseTime=true")
 
 	_, err = DB.Query("SELECT * FROM posts")
 	for err != nil {
 		log.Println("couldnt connect, waiting 20 seconds before retrying")
 		time.Sleep(20*time.Second)
-		DB, err = sql.Open("mysql", "root:root@tcp(172.28.1.3:3306)/postsDB?parseTime=true")
+		DB, err = sql.Open("mysql", "root:root@tcp(172.28.1.2:3306)/postsDB?parseTime=true")
 	}
 
 	return DB
